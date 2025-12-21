d30a33ce86026bb3e12a1a187323b1aeec04eedc
fulvio-ops <fulvio@fugallo.it>
2025-12-22 00:05:54 +0100

    Create config.js

---
+++ src/lib/config.js
@@ -0,0 +1,7 @@
+export const SITE = {
+  title: "KETOGO",
+  subtitle: "Weekly visual selection",
+  subreddits: ["Damnthatsinteresting", "oddlysatisfying", "InternetIsBeautiful", "DesignPorn"],
+  limit: 22,
+  maxAgeDays: 14
+};
